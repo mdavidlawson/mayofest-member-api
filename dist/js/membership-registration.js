2784387d806d002042395496438f413d60302623
mdavidlawson <mdavidlawson@gmail.com>
2017-03-17 07:23:00 -0400

    Put more work into the remote order intake form
    I keep hitting walls with Squarespace's support for custom code. Now it seems that the checkout page is off limits. Every page up to the checkout page is fine... but not the checkout page.
    
    I'm starting to wonder if this will be worth the work. There are many considerations to take when modifying the add to cart flow (cart abandondment, removing items, custom checkout forms). We may be closer to running our own portal now....

--- dist/js/membership-registration.js
+++ dist/js/membership-registration.js
@@ -1,14 +1,44 @@
+// "prepend event" functionality as a jQuery plugin
+$.fn.extend({
+  prependEvent: function (event, handler) {
+    return this.each(function () {
+      var events = $(this).data("events"),
+          currentHandler;
+
+      if (events && events[event].length > 0) {
+        currentHandler = events[event][0].handler;
+        events[event][0].handler = function () {
+          handler.apply(this, arguments);
+          currentHandler.apply(this, arguments);
+        }
+      }
+    });
+  }
+});
+
 $(document).ready(function(){
-  $("#test-registration-trigger").click(onRegistrationButtonClick);
+  $(".checkout-button").prependEvent("click", function(){
+    console.log("Checkout button clicked");
+    onRegistrationButtonClick();
+  });
 });
 
 function registerMember(member){
   console.log("Registering member")
 }
+function prepDialog(){
+  // TODO probably should be done by a templating engine
+  $(".modal-body").html($("#intake-container").html());
+}
+
 function raiseRegistrationDialog(){
-  
+  console.log("Raising dialog");
+  prepDialog();
+  $("#intake-model").modal();
 }
 
 function onRegistrationButtonClick(event){
-  console.log("registration click.");
+  if (! $("#intake-model").hasClass("in")) {
+    raiseRegistrationDialog();
+  }
 }
