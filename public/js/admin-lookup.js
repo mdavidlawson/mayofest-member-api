674dfaba507c79e4bd1be4bf8e3213a125b516b2
mdavidlawson <mdavidlawson@gmail.com>
2017-06-04 11:02:58 -0400

    Beginning to add actions to table

--- public/js/admin-lookup.js
+++ public/js/admin-lookup.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   _init_table();
+  _setup_add();
 });
 
 function _init_table(){
@@ -13,8 +14,81 @@ function _wrap_table(){
     "ajax": window.location.origin + "/api/member",
     "columns": [
         {"data": "memberNumber"},
-            {"data": "name"},
-                {"data": "email"}
+        {"data": "name"},
+        {"data": "email"},
+        {"data": "status"}
+    ],
+    select: "single",
+    dom: 'Bfrtip',
+    buttons: [
+        _make_custom_button("Reload", doReload),
+        _make_custom_button("Add", doAdd),
+        _make_custom_button("Edit", doEdit),
+        _make_custom_button("Remove", doRemove),
     ]
   });
+}
+function _setup_add(){
+  $("#save-changes").click(onAdd);
+}
+function _make_custom_button(text, handler){
+  return {
+      text: text,
+      action: handler
+  };
+}
+function _get_single_selected(){
+  var selectedItems = $("#member-table").DataTable().rows( { selected: true } ).data();
+  if (selectedItems.length === 0){
+    return null;
+  }
+  return selectedItems[0];
+}
+/** Handler Methods **/
+function doReload(e, dt, node, config){
+    dt.ajax.reload();
+}
+function doReload(){
+  $("#member-table").DataTable().ajax.reload();
+}
+function doAdd(e, dt, node, config){
+  $("#intake-model").modal({
+    show: true
+  });
+}
+function doRemove(e, dt, node, config){
+  var selectedItem = _get_single_selected();
+  if (!selectedItem){
+    console.log("Will not delete, nothing selected.");
+    return;
+  }
+  $('#confirm-delete').modal({
+      backdrop: 'static',
+      keyboard: false
+    })
+    .one('click', '#delete-member', function(e) {
+
+      $.ajax({
+        url: window.location.origin + "/api/member/"+selectedItem._id,
+        type: 'DELETE'
+      }).done(function(){
+        console.log("Deleted");
+        doReload();
+      });
+    });
+}
+function doEdit(e, dt, node, config){
+  console.log("Editing!");
+}
+function onAdd(e){
+  $.post(window.location.origin + "/api/member", $("#membership-intake").serialize())
+    .done(function() {
+      console.log("Success");
+      doReload();
+    })
+    .fail(function() {
+      console.log( "error" );
+    }).always(function(){
+      console.log( "done" );
+    });
 }
--- routes/pages/lookup.js
+++ routes/pages/lookup.js
@@ -5,10 +5,18 @@ exports.init = function(req, res){
       var scripts = [
         {script: 'js/admin-lookup.js'},
         {script: "bower_components/handlebars/handlebars.js"},
-        {script: "bower_components/datatables.net/js/jquery.dataTables.js"}
+        {script: "bower_components/datatables.net/js/jquery.dataTables.js"},
+        {script: "bower_components/datatables.net-buttons/js/dataTables.buttons.min.js"},
+        {script: "bower_components/datatables.net-buttons/js/buttons.print.min.js"},
+        {script: "bower_components/datatables.net-buttons/js/buttons.html5.min.js"},
+        {script: "bower_components/datatables.net-buttons/js/buttons.flash.min.js"},
+        {script: "bower_components/datatables.net-buttons/js/buttons.colVis.min.js"},
+        {script: "bower_components/datatables.net-select/js/dataTables.select.min.js"}
       ];
       var styles = [
-        {script: "bower_components/datatables.net-dt/css/jquery.dataTables.min.css"}
+        {script: "bower_components/datatables.net-dt/css/jquery.dataTables.min.css"},
+        {script: "bower_components/datatables.net-buttons-dt/css/buttons.dataTables.min.css"},
+        {script: "bower_components/datatables.net-select-dt/css/select.dataTables.min.css"}
       ]
       var context = {
         layoutData: {
