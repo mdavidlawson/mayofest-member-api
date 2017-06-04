8d412bfab03b8a85bd01e630cc3e8d4f5ac9a0f6
mdavidlawson <mdavidlawson@gmail.com>
2017-06-04 17:20:57 -0400

    Ensured kiosk doesn't display old entry info and that it redirects to self

--- routes/pages/kiosk.js
+++ routes/pages/kiosk.js
@@ -2,17 +2,9 @@
 var exports = module.exports;
 exports.init = function(req, res){
 
-      // var scripts = [
-      //   {script: 'js/admin-lookup.js'},
-      //   {script: "bower_components/handlebars/handlebars.js"},
-      //   {script: "bower_components/datatables.net/js/jquery.dataTables.js"},
-      //   {script: "bower_components/datatables.net-buttons/js/dataTables.buttons.min.js"},
-      //   {script: "bower_components/datatables.net-buttons/js/buttons.print.min.js"},
-      //   {script: "bower_components/datatables.net-buttons/js/buttons.html5.min.js"},
-      //   {script: "bower_components/datatables.net-buttons/js/buttons.flash.min.js"},
-      //   {script: "bower_components/datatables.net-buttons/js/buttons.colVis.min.js"},
-      //   {script: "bower_components/datatables.net-select/js/dataTables.select.min.js"}
-      // ];
+      var scripts = [
+        {script: 'js/kiosk.js'}
+      ];
       // var styles = [
       //   {script: "bower_components/datatables.net-dt/css/jquery.dataTables.min.css"},
       //   {script: "bower_components/datatables.net-buttons-dt/css/buttons.dataTables.min.css"},
@@ -21,7 +13,7 @@ exports.init = function(req, res){
       var context = {
         layoutData: {
         title: 'Self Serve Kiosk',
-        // scripts: scripts,
+        scripts: scripts,
         // styles: styles,
         meta: {
           author: 'Dave Lawson',
